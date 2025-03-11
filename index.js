492483977b99aa0e6b1d37c28bd3673a14385167
Raki6655 <alannpn75@gmail.com>
2025-03-11 15:11:09 +0530

    cors fix

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ mongoose
 
 app.use(express.json({ limit: "5mb" }));
 app.use(urlencoded({ extended: true }));
-app.use(
-	cors({
-		origin: "*",
-	})
-);
-app.get("/", (req, res) => {
-	res.send("<h1>Hello,Welcome to TweenLab</>");
+const corsOptions = {
+	origin: "*", // Allow all origins (you can replace with a specific origin)
+	methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+	allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+	credentials: true, // Allow cookies/auth headers
+};
+app.use(cors(corsOptions));
+
+app.use("/", (req, res) => {
+	res.send("<h1>Hello,Welcome Home</>");
 });
 app.use("/api", require("./route/auth"));
 app.use("/post", require("./route/post"));
@@ -26,6 +29,7 @@ app.use("/post", require("./route/post"));
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/.next"));
 }
+app.use(cors());
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
